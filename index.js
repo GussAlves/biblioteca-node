9df6e7e5d4f31250fc81ed9f43a7cdb09cfc00ea
Gustavo Alves de Araujo <gustavo.araujo@sbkbs.com.br>
2022-02-20 13:12:45 -0300

    Adicionando dependencias e entendendo promises

--- index.js
+++ index.js
@@ -1,9 +1,44 @@
-console.log('Iniciando o projeto');
+import chalk from "chalk";
+import fs from "fs";
 
-var paragrafo = 'Um texto bem bonito!';
-
-function texto (string) {
-    return string;
+function trataErro(erro) {
+    throw new Error(chalk.red(erro.code, 'não há arquivo no caminho'))
 }
 
-console.log(texto(paragrafo))
+//com async / await
+async function pegaArquivo(pathArquivo) {
+    const encoding = 'UTF-8'
+    try {
+        const texto = await fs.promises.readFile(pathArquivo, encoding)
+        console.log(chalk.green(texto))    
+    } catch (erro) {
+        trataErro(erro)
+    } finally {
+        console.log(chalk.blue('A operação foi finalizada!'))
+    }
+} 
+
+// com promisses 
+/* function pegaArquivo(pathArquivo) {
+    const encoding = 'UTF-8'
+    fs
+        .promises
+        .readFile(pathArquivo, encoding)
+        .then(x => console.log(chalk.green(x)))
+        .catch(err => trataErro(err))
+} */
+
+// Sem promises 
+/* function pegaArquivoSemPromises(pathArquivo) {
+    const encoding = 'UTF-8'
+    fs.readFile(pathArquivo, encoding, (err, texto) => {
+        if (err) {
+            trataErro(err)
+            return
+        }
+        console.log(chalk.green(texto))
+    })
+} */
+//pegaArquivoSemPromises('./arquivos/texto1.md')
+
+pegaArquivo('./arquivos/texto1.md')
